19ca3f46410c4f4fe57e450c7d89742489fe1f57
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-13 16:49:10 +0200

    Solved task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (theString, theNumber) {
+  let rezult = "";
 
+  if (theNumber >= 0) {
+    for (let i = 0; i < theNumber; i++) {
+      rezult += theString;
+    }
+  } else {
+    return "ERROR";
+  }
+
+  return rezult;
 };
 
 // Do not edit below this line
