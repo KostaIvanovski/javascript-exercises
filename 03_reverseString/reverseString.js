4859745c62d97522641fc4e981340309c250d743
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-13 17:01:58 +0200

    Solved task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (theString) {
+  let arr = theString.split("");
+  return arr.reverse().join("");
 };
 
 // Do not edit below this line
