b96b1a48f3e0ed69ed6194aa3331bfd477ee9f44
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-15 12:07:17 +0200

    Solved task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let rezult = false;
+  if (year % 4 === 0) rezult = true;
+  if (year % 100 === 0) rezult = false;
+  if (year % 400 === 0) rezult = true;
 
+  return rezult;
 };
 
 // Do not edit below this line
