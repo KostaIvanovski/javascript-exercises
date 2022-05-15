1d95539c710e41f932247aed565916fbbaf78753
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-15 11:22:40 +0200

    Solved task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  if (arguments.length === 0) return;
 
+  let rezult = arguments[0];
+
+  for (let i = 1; i < arguments.length; i++) {
+    let index = rezult.findIndex((element) => element === arguments[i]);
+    if (index >= 0) rezult.splice(index, 1);
+  }
+
+  return rezult;
 };
 
 // Do not edit below this line
