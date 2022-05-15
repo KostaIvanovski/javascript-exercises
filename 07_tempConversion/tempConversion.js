5b7a9835735fa423877eb424b3aa0d8a32892483
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-15 12:34:13 +0200

    Solved task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  let rezult = Math.round((((fahrenheit - 32) * 5) / 9) * 10) / 10;
+  return rezult;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let rezult = Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+  return rezult;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
