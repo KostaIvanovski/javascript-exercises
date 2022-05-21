b4b7ad691e4c012cfd14fe4b8f2a744b96cca935
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-21 20:29:45 +0200

    Cant solve task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let indexOfOldest = 0;
 
+  for (let i = 0; i < array.length; i++) {
+    if ("yearOfDeath" in array[i]) {
+      if (array[i]["yearOfDeath"] > array[indexOfOldest]["yearOfDeath"]) {
+        indexOfOldest = i;
+      }
+    } else if (array[i]["yearOfBirth"] < 1900) {
+      indexOfOldest = i;
+    }
+    return array[indexOfOldest];
+  }
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
