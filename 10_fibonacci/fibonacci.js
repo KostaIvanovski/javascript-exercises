5111143b8c77ff51d6ab62502b0ae7d8de58eac5
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-21 17:24:56 +0200

    Solved task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  let numPrev = 0;
+  let numNext = 1;
+  for (let i = 0; i < parseInt(num); i++) {
+    let temp = numNext;
+    numNext += numPrev;
+    numPrev = temp;
+  }
 
+  return numPrev;
 };
 
 // Do not edit below this line
