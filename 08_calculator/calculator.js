775895f219ed279fd3d4521ea9ba368ed000b6d5
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-21 16:40:57 +0200

    Solved task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let rezult = 1;
+  for (let i = 0; i < array.length; i++) {
+    rezult *= array[i];
+  }
+  return rezult;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let rezult = 1;
+  for (let i = num; i > 0; i--) {
+    rezult *= i;
+  }
+  return rezult;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
