6d7fa2c6744b6505cf47b05473ba91d761fb1b7a
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-21 17:13:22 +0200

    Solved task 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let arr = string
+    .toLowerCase()
+    .split(/[^a-zA-Z]+/)
+    .join("")
+    .split("");
+  let palindrome = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[arr.length - 1 - i]) {
+      palindrome = true;
+    } else {
+      palindrome = false;
+      return palindrome;
+    }
+  }
+  return palindrome;
 };
 
 // Do not edit below this line
