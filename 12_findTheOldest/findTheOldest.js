52c400c5f6349c5bd7bf9a96931ca4fcee153e8b
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-22 13:24:59 +0200

    Solved task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,22 @@
 const findTheOldest = function (array) {
-  let indexOfOldest = 0;
-
+  let ageOfOldest = 0;
+  let indexOfOldest;
+  let date = new Date();
+  let currentYear = date.getFullYear();
   for (let i = 0; i < array.length; i++) {
-    if ("yearOfDeath" in array[i]) {
-      if (array[i]["yearOfDeath"] > array[indexOfOldest]["yearOfDeath"]) {
-        indexOfOldest = i;
-      }
-    } else if (array[i]["yearOfBirth"] < 1900) {
+    let age;
+    if (!array[i]["yearOfDeath"]) {
+      age = currentYear - array[i]["yearOfBirth"];
+    } else {
+      age = array[i]["yearOfDeath"] - array[i]["yearOfBirth"];
+    }
+    if (age > ageOfOldest) {
+      ageOfOldest = age;
       indexOfOldest = i;
     }
-    return array[indexOfOldest];
   }
+  return array[indexOfOldest];
 };
+
 // Do not edit below this line
 module.exports = findTheOldest;
